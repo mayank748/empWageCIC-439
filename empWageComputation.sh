25114fac6a4992d5488cf9d925b9aa3ec277dc6a
Mayank <smayank748@gmail.com>
2022-01-31 08:55:34 +0530

    [Mayank]added the emp attandance

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,1 +1,13 @@
+#! /bin/bash -x
+
 echo "Welcome to employee wage computation program on Master Branch"
+
+IS_PRESENT=1
+IS_ABSENT=0
+randomValue=$((RANDOM%2))
+if [ $randomValue -eq $IS_PRESENT ]
+then
+	echo "Employee is presnet."
+else
+	echo "Employee is absent."
+fi
