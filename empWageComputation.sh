59ef580a68d8f5a09b73b9369d63903ffc002f03
Mayank <smayank748@gmail.com>
2022-01-31 09:11:07 +0530

    [Mayank]added the daily wage for employee

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -5,9 +5,22 @@ echo "Welcome to employee wage computation program on Master Branch"
 IS_PRESENT=1
 IS_ABSENT=0
 randomValue=$((RANDOM%2))
+totalSalary=0
+TOTAL_WORKING_HOUR=8
+AMOUNT_PAID_PER_HOUR=20
+
+
+function salaryFunction(){
+	local totalWorkingHour=$1
+	local amountPaidPerHour=$2
+	local totalAmount=$(($totalWorkingHour*$amountPaidPerHour))
+	echo $totalAmount
+}
+
 if [ $randomValue -eq $IS_PRESENT ]
 then
 	echo "Employee is presnet."
+	echo "total amount " `salaryFunction TOTAL_WORKING_HOUR AMOUNT_PAID_PER_HOUR`
 else
 	echo "Employee is absent."
 fi
