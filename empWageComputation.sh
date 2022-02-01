239776d111cb4579a9ca4cf79ef0be0ba6b48673
Mayank <smayank748@gmail.com>
2022-02-01 08:50:41 +0530

    calculated the wage for month

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -5,11 +5,12 @@ echo "Welcome to employee wage computation program on Master Branch"
 IS_PRESENT=1
 IS_ABSENT=0
 IS_PART_TIME=2
-randomValue=$((RANDOM%3))
-totalSalary=0
 TOTAL_WORKING_HOUR=8
 AMOUNT_PAID_PER_HOUR=20
 TOTAL_WORKING_HOUR_FOR_PART_TIME=8
+TOTAL_WORKING_DAYS=20
+toatalMonthSalary=0
+
 
 function salaryFunction(){
 	local totalWorkingHour=$1
@@ -18,13 +19,25 @@ function salaryFunction(){
 	echo $totalAmount
 }
 
+while [ $TOTAL_WORKING_DAYS -gt 0 ]
+do
+
+randomValue=$((RANDOM%3))
+totalSalary=0
+
 case $randomValue in 
 	$IS_PRESENT)
 	echo "Employee is presnet."
-	echo "total amount " `salaryFunction TOTAL_WORKING_HOUR AMOUNT_PAID_PER_HOUR`;;
+	totalSalary=`salaryFunction TOTAL_WORKING_HOUR AMOUNT_PAID_PER_HOUR`;;
 	$IS_PART_TIME)
 	echo "Employee is doing parttime."
-	echo "total amount " `salaryFunction TOTAL_WORKING_HOUR_FOR_PART_TIME AMOUNT_PAID_PER_HOUR`;;
+	totalSalary=`salaryFunction TOTAL_WORKING_HOUR_FOR_PART_TIME AMOUNT_PAID_PER_HOUR`;;
 	*)
 	echo "Employee is absent."
 esac
+
+toatalMonthSalary=$(($toatalMonthSalary+$totalSalary))
+((TOTAL_WORKING_DAYS--))
+done
+
+echo "Total month salary "$toatalMonthSalary
